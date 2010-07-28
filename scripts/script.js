8c688be2d21f1fec8a22286331a9ec5a28e82fbc
Ben Hunsaker <hunsaker@tamhsc.edu>
2010-07-28 10:16:08 -0500

    Version 1 updated, working in firefox and Safari, (probably chrome).

--- scripts/script.js
+++ scripts/script.js
@@ -22,29 +22,34 @@ $(document).ready(function(){
 	
 	var numbers = new Array('zero','one','two','three','four','five','six','seven','eight','nine');
 	
+	var clock_container = $('#main');
+	
 	clock.hour.first.addClass("hour");
 	clock.hour.second.addClass("hour");
 	clock.day.first.addClass("day");
 	
-	$("#main").append(clock.day.first);
-	$("#main").append(clock.hour.first);
-	$("#main").append(clock.hour.second);
-	$("#main").append(clock.min.first);
-	$("#main").append(clock.min.second);
-	$("#main").append(clock.sec.first);
-	$("#main").append(clock.sec.second);
+	clock_container.append(clock.day.first);
+	clock_container.append(clock.hour.first);
+	clock_container.append(clock.hour.second);
+	clock_container.append(clock.min.first);
+	clock_container.append(clock.min.second);
+	clock_container.append(clock.sec.first);
+	clock_container.append(clock.sec.second);
 	
-	$("#main").append($('<section id="day_divider" class="clock_divider">:</section>'));
-	$("#main").append($('<section id="hour_divider" class="clock_divider">:</section>'));
-	$("#main").append($('<section id="min_divider" class="clock_divider">:</section>'));
+	clock_container.append($('<section id="day_divider" class="clock_divider">:</section>'));
+	clock_container.append($('<section id="hour_divider" class="clock_divider">:</section>'));
+	clock_container.append($('<section id="min_divider" class="clock_divider">:</section>'));
 	
-	$("#main").append($('<section id="days_label" class="label">Days</section>'));
-	$("#main").append($('<section id="hours_label" class="label">Hours</section>'));
-	$("#main").append($('<section id="minutes_label" class="label">Minutes</section>'));
-	$("#main").append($('<section id="seconds_label" class="label">Seconds</section>'));
+	clock_container.append($('<section id="days_label" class="label">Days</section>'));
+	clock_container.append($('<section id="hours_label" class="label">Hours</section>'));
+	clock_container.append($('<section id="minutes_label" class="label">Minutes</section>'));
+	clock_container.append($('<section id="seconds_label" class="label">Seconds</section>'));
 	
 	var Today = new Date();
 	var days_till_friday = 5 - Today.getDay();
+	if(days_till_friday < 0){
+		days_till_friday = 6;
+	}
 	var Friday = new Date()
 	
 	Friday.setDate(Today.getDate() + days_till_friday);
@@ -53,92 +58,101 @@ $(document).ready(function(){
 	Friday.setSeconds(00);
 	
 	var time_left = (Friday - Today)/1000;		//in seconds
-
-	var days_left = time_left/(3600 * 24);
-	init_number(days_left, clock.day);
+	if(time_left < 0){
+		start_dancing();
+	}
+	else{
+		var days_left = time_left/(3600 * 24);
+		init_number(days_left, clock.day);
 	
-	var hours_left = (time_left%(3600 * 24))/3600;
-	init_number(hours_left, clock.hour);
+		var hours_left = (time_left%(3600 * 24))/3600;
+		init_number(hours_left, clock.hour);
 	
-	var min_left = (time_left%3600)/60;
-	init_number(min_left, clock.min);
+		var min_left = (time_left%3600)/60;
+		init_number(min_left, clock.min);
 	
-	var sec_left = (time_left%3600)%60;
-	init_number(sec_left, clock.sec);
+		var sec_left = (time_left%3600)%60;
+		init_number(sec_left, clock.sec);
 	
-	change_clock();
+		change_clock();
+	}
 	
 	function change_clock(){
-		countdown(clock.sec.second);
-		setTimeout(change_clock,1000)
-	}
-	
-	function countdown(number){
-		var new_num = $('div',number).first().text() - 1;
-		if(new_num < 0){
-			if(number.hasClass('second')){
-				if(number.hasClass('hour')){
-					flip_number(number, 3);
-				}
-				else{
-					flip_number(number, 9);
-				}
-			}
-			else{
-				if(number.hasClass('hour')){
-					flip_number(number, 2);
-				}
-				else{
-					flip_number(number, 5);
-				}
-			}
-			countdown(number.prev('section.number:first'));
-		}
-		else
-			flip_number(number, new_num);
-	}
-	
-	function flip_number(number, new_number){
-		var num = {
-			upper: $('div.upper_number:first', number),
-			lower: $('div.lower_number:first', number)
+			countdown(clock.sec.second);
+			var now = new Date();
+			
+			if(Friday - now > 1000)
+				setTimeout(change_clock,1000);
+			else
+				setTimeout(start_dancing, 2000);
 		}
 		
-		var upClone = num.upper.clone();
-		var lowClone = num.lower.clone();
-		lowClone.css('height','0px');
-		number.append(upClone);
-		number.append(lowClone);
-		
-		for(n in numbers){
-			num.upper.removeClass(numbers[n]);
-			lowClone.removeClass(numbers[n]);
-		}
-		
-		num.upper.text(new_number);
-		num.upper.addClass(numbers[new_number]);
-		
-		lowClone.text(new_number);
-		lowClone.addClass(numbers[new_number]);
-		
-		upClone.animate({'height': '0px','top': num.upper.innerHeight() + "px"}, {
-			duration: 400,
-			queue: false,
-			easing: 'easeInQuart',
-			complete: function (){
-				lowClone.animate({'height': num.lower.innerHeight() + "px"}, {
-					duration: 400,
-					queue: false,
-					easing: 'easeOutBounce',
-					complete: function(){
-						upClone.detach();
-						num.lower.detach();
+		function countdown(number){
+			var new_num = $('div',number).first().text() - 1;
+			if(new_num < 0){
+				if(number.hasClass('second')){
+					if(number.hasClass('hour')){
+						flip_number(number, 3);
 					}
-				});
+					else{
+						flip_number(number, 9);
+					}
+				}
+				else{
+					if(number.hasClass('hour')){
+						flip_number(number, 2);
+					}
+					else{
+						flip_number(number, 5);
+					}
+				}
+				countdown(number.prev('section.number:first'));
 			}
-		});
+			else
+				flip_number(number, new_num);
+		}
 		
-	}
+		function flip_number(number, new_number){
+			var num_parts = {
+				upper: $('div.upper_number:first', number),
+				lower: $('div.lower_number:first', number)
+			}
+			
+			var upClone = num_parts.upper.clone();
+			var lowClone = num_parts.lower.clone();
+			lowClone.css('height','0px');
+			number.append(upClone);
+			number.append(lowClone);
+			
+			for(n in numbers){
+				num_parts.upper.removeClass(numbers[n]);
+				lowClone.removeClass(numbers[n]);
+			}
+			
+			num_parts.upper.text(new_number);
+			num_parts.upper.addClass(numbers[new_number]);
+			
+			lowClone.text(new_number);
+			lowClone.addClass(numbers[new_number]);
+			upClone.css({'top': '4px'})
+			
+			upClone.animate({'height': '0px','top': num_parts.upper.innerHeight() + "px"}, {
+				duration: 400,
+				queue: false,
+				easing: 'easeInQuart',
+				complete: function (){
+					lowClone.animate({'height': num_parts.lower.innerHeight() + "px"}, {
+						duration: 400,
+						queue: false,
+						easing: 'easeOutBounce',
+						complete: function(){
+							upClone.detach();
+							num_parts.lower.detach();
+						}
+					});
+				}
+			});		
+		}
 	
 	function init_number(number, num_container){
 		var i = number.toString();
@@ -153,14 +167,21 @@ $(document).ready(function(){
 			integer = "0" + integer;
 		}
 		
-		$('div', num_container.first).text(integer[0]);
-		$('div', num_container.first).addClass(numbers[integer[0]]);
+		var first_num = $('div.upper_number, div.lower_number', num_container.first);
+		$(first_num).text(integer[0]);
+		first_num.addClass(numbers[integer[0]]);
 		
 		if(num_container.second){
-			$('div', num_container.second).text(integer[1]);
-			$('div', num_container.second).addClass(numbers[integer[1]]);
+			var second_num = $('div.upper_number, div.lower_number', num_container.second);
+			second_num.text(integer[1]);
+			second_num.addClass(numbers[integer[1]]);
 		}
-	}	
+	}
+	
+	function start_dancing(){
+		clock_container.empty();
+		clock_container.attr('id','happy_dance');
+	}
 });
 
 
